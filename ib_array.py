6fc77b6cd649e8d9468d611ddeafd002fb184dc8
jegstar <wjeggo@hotmail.com>
2023-04-12 07:30:58 +0800

    added to runner

--- ib_array.py
+++ ib_array.py
@@ -1,18 +1,55 @@
 class Array:
-    def __init__(self, size):
-        self._data = [0] * size
+    def __init__(self, size=10, type="int"):
+        if type not in ['int', 'float', 'char', 'string', 'bool']:
+            raise TypeError ("Can only accept int, float, char, bool or string")
+        
+        self._type = type
+        if type == 'int':
+            self._data = [0] * size
+
+        if type == 'float':
+            self._data = [0.0] * size
+
+        if type == 'char':
+            self._data = [' '] * size
+
+        if type == 'string':
+            self._data = [""] * size
+
+        if type == 'bool':
+            self._data = [True] * size
 
     def __getitem__(self, key):
         if key < 0 or key >= len(self._data):
             raise IndexError("Out of range")
-        else:
-            return self._data[key]
+        
+        return self._data[key]
 
     def __setitem__(self, key, value):
+        print(self._type)
         if key < 0 or key >= len(self._data):
             raise IndexError("Out of range")
-        else:
+        
+        elif isinstance(value, int) and self._type == 'int':
             self._data[key] = value
+    
+        elif isinstance(value, float) and self._type == 'float':
+            self._data[key] = value
+    
+        elif isinstance(value, bool) and self._type == 'bool':
+            self._data[key] = value
+    
+        elif isinstance(value, str) and self._type == 'string':
+            self._data[key] = value
+    
+        elif isinstance(value, str) and self._type == 'char':
+            if len(value) == 1:
+                self._data[key] = value
+            else:
+                raise TypeError("Char must be length 1")
+        
+        else:
+            raise TypeError("Invalid input type. This Array only accepts " + self._type)
     
     def __str__(self):
         return str(self._data)
--- runner.py
+++ runner.py
@@ -1,13 +1,39 @@
 from ib_array import Array
-
-arr = Array(10)
-
-print(arr)
-
 from ib_collection import Collection
 
+# create an empty array of strings
+string_arr = Array(5, 'string')
+print(string_arr)
+
+# change item in array
+string_arr[2] = "New value"
+print(string_arr)
+
+# loop through array
+for item in string_arr:
+    print(item)
+
+
+
+# Collections
 col = Collection()
 
-col.addItem(3)
+print(col.isEmpty())
+
+col.addItem("a")
+col.addItem("b")
+col.addItem("c")
 
 print(col)
+while col.hasNext():
+    print(col.getNext())
+
+
+######### Collections problems ###############
+# Write a method that inputs a persons expenses as a float and expense class as a string.
+# EG 
+# 2.65, travel
+# 4.99, food
+# The user has the following classes available: food, travel, housing, clothing, other
+# Add each to a collection for its correct type.
+# Then write an algorithm to calculate the number and total value of each type.
